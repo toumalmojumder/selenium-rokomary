7eb52a2bab7c9cf46ae68cccaf4a725d7a5869b7
Toumal Mojumder <32560686+toumalmojumder@users.noreply.github.com>
2021-11-16 04:25:03 +0600

    file rearrange

--- rokomaryProject/main.py
+++
@@ -1,9 +0,0 @@
-import time
-import unittest
-from selenium import webdriver
-
-driver = webdriver.Chrome("C:\\chromedriver_win32\\chromedriver.exe")
-driver.get("https://www.rokomari.com/book")
-driver.close()
-
-
